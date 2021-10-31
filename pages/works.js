c39a1ca62e54fcde6c2047c7add8a48ae6a3cfc7
DavidReider <freakdj1997@yahoo.com>
2021-10-31 19:27:08 -0400

    Add Projects Page

---
+++ pages/works.js
@@ -0,0 +1,59 @@
+import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumbCodebug from '../public/images/works/Codebug.jpg'
+import thumbYota from '../public/images/works/yota.jpg'
+
+const Works = () => {
+    return (
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Works
+            </Heading>
+            <SimpleGrid columns={[1,1,2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
+                        A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
+                        An online eCommerce website created with Wix CMS for my family&apos;s business.
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Collaborations
+            </Heading>
+            <SimpleGrid columns={[1,1,2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
+                        A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
+                        An online eCommerce website created with Wix CMS for my family&apos;s business.
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Old Projects
+            </Heading>
+            <SimpleGrid columns={[1,1,2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
+                        A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
+                        An online eCommerce website created with Wix CMS for my family&apos;s business.
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+        </Container>
+    )
+}
+
+export default Works
