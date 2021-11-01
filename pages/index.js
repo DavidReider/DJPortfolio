e95e432f484237b24d7c11a172b7e0255246ba7a
DavidReider <freakdj1997@yahoo.com>
2021-11-01 18:15:46 -0400

    Add Transitions when swapping between pages

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,61 @@
 import NextLink from 'next/link'
 import { Button, Container, Box, Heading, useColorModeValue } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
     return (
-        <Container>
-            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
-                Hello, I&apos;m a web developer based in the United States.
-            </Box>
-            <Box display={{md: 'flex'}}>
-                <Box flexGrow={1}>
-                    <Heading as="h2" variant="page-title">
-                        DJ Reider
+        <Layout>
+            <Container>
+                <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
+                    Hello, I&apos;m a web developer based in the United States.
+                </Box>
+                <Box display={{md: 'flex'}}>
+                    <Box flexGrow={1}>
+                        <Heading as="h2" variant="page-title">
+                            DJ Reider
+                        </Heading>
+                        <p> HTML / CSS / JavaScript </p>
+                    </Box>
+                </Box>
+                <Section delay={0.1}>
+                    <Heading as="h3" variant="section-title">
+                        About
                     </Heading>
-                    <p> HTML / CSS / JavaScript </p>
-                </Box>
-            </Box>
-            <Section delay={0.1}>
-                <Heading as="h3" variant="section-title">
-                    About
-                </Heading>
-                <Paragraph> I am a Web Developer based in Pennsylvania, USA with a passion for building digital services and other various projects. I enjoy launching products and planning and designing solutions for real-life problems with code. </Paragraph>
-                <Box align="center" my={4}>
-                    <NextLink href="/works">
-                        <Button rightIcon={<ChevronRightIcon />} colorScheme="teal"> My Work </Button>
-                    </NextLink>
-                </Box>
-            </Section>
+                    <Paragraph> I am a Web Developer based in Pennsylvania, USA with a passion for building digital services and other various projects. I enjoy launching products and planning and designing solutions for real-life problems with code. </Paragraph>
+                    <Box align="center" my={4}>
+                        <NextLink href="/works">
+                            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal"> My Work </Button>
+                        </NextLink>
+                    </Box>
+                </Section>
 
-            <Section delay={0.2}>
-                <Heading as="h3" variant="section-title">
-                    Bio
-                </Heading>
-                <BioSection>
-                    <BioYear> 1997 </BioYear>
-                    Born in Harrisburg, Pennsylvania.
-                </BioSection>
-                <BioSection>
-                    <BioYear> 2019 </BioYear>
-                    Worked at Penn State Applied Research Lab. 
-                </BioSection>
-                <BioSection>
-                    <BioYear> 2020 </BioYear>
-                    Graduated from Widener University with a Bachelor of Science Degree in Computer Science. 
-                </BioSection>
-                <BioSection>
-                    <BioYear> 2020 to present</BioYear>
-                    Work at WebFX.
-                </BioSection>
-            </Section>
-        </Container>
-        
+                <Section delay={0.2}>
+                    <Heading as="h3" variant="section-title">
+                        Bio
+                    </Heading>
+                    <BioSection>
+                        <BioYear> 1997 </BioYear>
+                        Born in Harrisburg, Pennsylvania.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear> 2019 </BioYear>
+                        Worked at Penn State Applied Research Lab. 
+                    </BioSection>
+                    <BioSection>
+                        <BioYear> 2020 </BioYear>
+                        Graduated from Widener University with a Bachelor of Science Degree in Computer Science. 
+                    </BioSection>
+                    <BioSection>
+                        <BioYear> 2020 to present</BioYear>
+                        Work at WebFX.
+                    </BioSection>
+                </Section>
+            </Container>
+        </Layout>
     )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,66 +1,69 @@
-import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
+import Layout from '../components/layouts/article'
 import { WorkGridItem } from '../components/grid-item'
 import thumbCodebug from '../public/images/works/Codebug.jpg'
 import thumbYota from '../public/images/works/yota.jpg'
 
 const Works = () => {
     return (
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
-            <SimpleGrid columns={[1,1,2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
-                        A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
-                        An online eCommerce website created with Wix CMS for my family&apos;s business.
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-
-            <Section delay={0.2}>
-                <Divider my={6} />
+        <Layout>
+            <Container>
                 <Heading as="h3" fontSize={20} mb={4}>
-                    Collaborations
+                    Works
                 </Heading>
-            </Section>
-            <SimpleGrid columns={[1,1,2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
-                        A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
-                        An online eCommerce website created with Wix CMS for my family&apos;s business.
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
+                <SimpleGrid columns={[1,1,2]} gap={6}>
+                    <Section>
+                        <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
+                            A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
+                            An online eCommerce website created with Wix CMS for my family&apos;s business.
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
 
-            <Section delay={0.4}>
-                <Divider my={6} />
-                <Heading as="h3" fontSize={20} mb={4}>
-                    Old Projects
-                </Heading>
-            </Section>
-            <SimpleGrid columns={[1,1,2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
-                        A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
-                    </WorkGridItem>
+                <Section delay={0.2}>
+                    <Divider my={6} />
+                    <Heading as="h3" fontSize={20} mb={4}>
+                        Collaborations
+                    </Heading>
                 </Section>
-                <Section>
-                    <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
-                        An online eCommerce website created with Wix CMS for my family&apos;s business.
-                    </WorkGridItem>
+                <SimpleGrid columns={[1,1,2]} gap={6}>
+                    <Section>
+                        <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
+                            A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
+                            An online eCommerce website created with Wix CMS for my family&apos;s business.
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
+
+                <Section delay={0.4}>
+                    <Divider my={6} />
+                    <Heading as="h3" fontSize={20} mb={4}>
+                        Old Projects
+                    </Heading>
                 </Section>
-            </SimpleGrid>
-        </Container>
+                <SimpleGrid columns={[1,1,2]} gap={6}>
+                    <Section>
+                        <WorkGridItem id="codebug" title="Codebug" thumbnail={thumbCodebug}>
+                            A sudo blog that I&apos;ve created to practice learning with React and Sanity.IO CMS.
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="yotabearingsandhubs" title="Yota Bearings and Hubs" thumbnail={thumbYota}>
+                            An online eCommerce website created with Wix CMS for my family&apos;s business.
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
+            </Container>
+        </Layout>
     )
 }
 
