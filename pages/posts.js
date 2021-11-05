74df514b79fe9cdd844c202989174326900e39cc
DavidReider <freakdj1997@yahoo.com>
2021-11-05 18:10:37 -0400

    Add Posts Page

---
+++ pages/posts.js
@@ -0,0 +1,24 @@
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+
+import thumbYotaBearing from '../public/images/contents/yota.jpg'
+
+
+const Posts = () => (
+    <Layout title="Posts">
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Popular Posts
+            </Heading>
+            <Section delay={0.1}>
+                <SimpleGrid columns={[1,2,2]} gap={6}>
+                    <GridItem title="YotaBearingsAndHubs Wix" thumbnail={thumbYotaBearing} href="https://www.yotabearingsandhubs.com/" />
+                </SimpleGrid>
+            </Section>
+        </Container>
+    </Layout>
+)
+
+export default Posts
